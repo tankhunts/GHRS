87d30f7e0ed91b03153b8d8d74b6873e53947337
Matthew <mattd99@vt.edu>
2020-11-06 01:57:54 -0500

    Added the functionality to search through notes

--- searchNotes.py
+++ searchNotes.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import (QComboBox, QApplication, QMainWindow, QLabel, QWidget, QPushButton, QHBoxLayout, QLineEdit, QVBoxLayout, QScrollArea)
 from PyQt5.QtCore import pyqtSignal
+import pymongo
+from bson.objectid import ObjectId
 
 class searchNotes(QWidget):
     
@@ -11,32 +13,50 @@ class searchNotes(QWidget):
     search = QLineEdit()
     scrollData = QVBoxLayout()
     ex = pyqtSignal()
-    op = pyqtSignal(str)
-    comboData = ["Date", "Keyword"]
+    op = pyqtSignal(dict)
+    comboData = ["Date", "Keyword", "Subject"]
 
     def return_search_results(self, criteria, text):
         results = []
-        
+        myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+        mydb = myclient["GHRS"]
+        mycol = mydb["PatientData"]
+        notes = mycol.find_one({'_id': ObjectId(self.currId)})["Notes"]
+        if(criteria == 'Keyword'):
+            criteria = "Note"
+        if(text != ''):
+            for note in notes:
+                if text.lower() in note[criteria].lower():
+                    results.append(note)
+        else:
+            results = notes
+        results = sorted(results, key = lambda i: i[criteria])
         return results
         
-    def open_profile(self, name):
-        self.op.emit(name)
+    def open_note(self, noteDict):
+        self.op.emit(noteDict)
         
-    def create_result(self, name, date):
+    def create_result(self, noteDict):
         layout = QHBoxLayout()
-        text = QLineEdit(name)
-        age = QLineEdit(str(date))
+        text = QLineEdit(noteDict["Subject"])
+        age = QLineEdit(noteDict["Date"])
         ope = QPushButton("Open")
         
         text.setReadOnly(True)
         age.setReadOnly(True)
-        ope.clicked.connect(lambda: self.open_profile(name))
+        ope.clicked.connect(lambda: self.open_note(noteDict))
         
         layout.addWidget(text)
         layout.addWidget(age)
         layout.addWidget(ope)
         return layout
     
+    def clear(self):
+        self.clear_layout(self.scrollData)
+        self.search.clear()
+        self.criteria.clear()
+        self.criteria.addItems(self.comboData)
+
     #code taken from https://stackoverflow.com/questions/9374063/remove-all-items-from-a-layout
     def clear_layout(self, layout):
         if(layout is not None):
@@ -50,9 +70,9 @@ class searchNotes(QWidget):
     
     def begin_search(self):
         self.clear_layout(self.scrollData)
-        data = self.return_search_results(self.criteria.currentData(), self.search.text())
+        data = self.return_search_results(self.criteria.currentText(), self.search.text())
         for item in data:
-            self.scrollData.addLayout(self.create_result(item["name"], item["age"]))
+            self.scrollData.addLayout(self.create_result(item))
         
     def back(self):
         self.criteria.clear()
