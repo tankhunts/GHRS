cc1c73f372bd6a6900202231b301431c92f9df3d
Matthew <mattd99@vt.edu>
2020-11-17 21:37:22 -0500

    Added export button to main menu

--- menu.py
+++ menu.py
@@ -14,15 +14,21 @@ class MainMenu(QWidget):
     optionsLayout = QHBoxLayout()
     nameLabel = QLabel()
     mainLayout = QVBoxLayout(mainWidget)
+    
     #main menu buttons
     addPatientButton = QPushButton('Add patient')
     searchButton = QPushButton('Search for a profile')
+    exportButton = QPushButton("Export Data")
     search = pyqtSignal()
-    add = pyqtSignal()   
+    add = pyqtSignal()
+    export = pyqtSignal()
+
     def goAdd(self):
         self.add.emit()
     def goSearch(self):
         self.search.emit()
+    def goExport(self):
+        self.export.emit()
 
     def __init__(self, parent=None):
         super().__init__() 
@@ -36,8 +42,10 @@ class MainMenu(QWidget):
         self.mainLayout.addWidget(self.nameLabel)
         self.mainLayout.addStretch()
         self.mainLayout.addLayout(self.optionsLayout)
+        self.mainLayout.addWidget(self.exportButton)
         self.mainLayout.addStretch()
         self.setLayout(self.mainLayout)
 
+        self.exportButton.clicked.connect(self.goExport)
         self.addPatientButton.clicked.connect(self.goAdd)
         self.searchButton.clicked.connect(self.goSearch) 
