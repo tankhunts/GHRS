83c3b42d3755dbeeb4fafd00e0cd53cd80d60888
Matthew <mattd99@vt.edu>
2020-11-29 14:41:35 -0500

    Added importing functionality

---
+++ importing.py
@@ -0,0 +1,13 @@
+import csv
+import pymongo
+
+def readFile(filename):
+    myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+    mydb = myclient["GHRS"]
+    mycol = mydb["PatientData"]
+    patientList = []
+    with open(str(filename[0]), mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            patientList.append(row)
+    mycol.insert_many(patientList)
--- menu.py
+++ menu.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+import importing
+import pathlib
 
 class MainMenu(QWidget):
 
@@ -19,13 +21,16 @@ class MainMenu(QWidget):
     addPatientButton = QPushButton('Add patient')
     searchButton = QPushButton('Search for a profile')
     exportButton = QPushButton("Export Data")
+    importButton = QPushButton("Import Data")
     search = pyqtSignal()
     add = pyqtSignal()
     export = pyqtSignal()
+    impor = pyqtSignal()
 
     new = QGroupBox("For new customers")
     returning = QGroupBox("For returning customers")
     exporting = QGroupBox("Shipping data off")
+    importing = QGroupBox("Import data")
 
     def goAdd(self):
         self.add.emit()
@@ -33,10 +38,18 @@ class MainMenu(QWidget):
         self.search.emit()
     def goExport(self):
         self.export.emit()
+    def goImport(self):
+        self.impor.emit()
 
+    def getFile(self):
+        print(pathlib.Path().absolute())
+        fname = QFileDialog.getOpenFileName(self, 'Open file', str(pathlib.Path().absolute()), 'csv(*.csv)')
+        importing.readFile(fname)
     def __init__(self, parent=None):
         super().__init__() 
         #creating window
+
+
 
         self.nameLabel.setText(self.name)
         self.nameLabel.setAlignment(Qt.AlignCenter)
@@ -46,10 +59,12 @@ class MainMenu(QWidget):
         layout1 = QVBoxLayout()
         layout2 = QVBoxLayout()
         layout3 = QVBoxLayout()
+        layout4 = QVBoxLayout()
 
         layout1.addWidget(self.addPatientButton)
         layout2.addWidget(self.searchButton)
         layout3.addWidget(self.exportButton)
+        layout4.addWidget(self.importButton)
 
 
 
@@ -57,14 +72,17 @@ class MainMenu(QWidget):
         self.new.setLayout(layout1)
         self.returning.setLayout(layout2)
         self.exporting.setLayout(layout3)
+        self.importing.setLayout(layout4)
         self.mainLayout.addWidget(self.nameLabel)
         #self.mainLayout.addStretch()
         self.mainLayout.addWidget(self.new)
         self.mainLayout.addWidget(self.returning)
         self.mainLayout.addWidget(self.exporting)
+        self.mainLayout.addWidget(self.importing)
         #self.mainLayout.addStretch()
         self.setLayout(self.mainLayout)
 
         self.exportButton.clicked.connect(self.goExport)
         self.addPatientButton.clicked.connect(self.goAdd)
         self.searchButton.clicked.connect(self.goSearch) 
+        self.importButton.clicked.connect(self.getFile)
